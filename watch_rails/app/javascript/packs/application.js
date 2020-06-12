a7fd5a41ce33b05c1a70fff1ef060d6cfa256a62
SinkLineP <popovartem505@gmail.com>
2020-06-12 22:58:00 +0300

    15:30 fix Style Done

--- watch_rails/app/javascript/packs/application.js
+++ watch_rails/app/javascript/packs/application.js
@@ -7,30 +7,30 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
+require("jquery")
+import 'bootstrap'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
-import '../stylesheets/application'
-import '../stylesheets/main'
+// import 'bootstrap/dist/js/bootstrap'
+// import 'bootstrap/dist/css/bootstrap'
+import '../stylesheets/application.scss'
 
 
-import "../styles/application";
-import './jqery-1.11.0.min.js'
-import './bootstrap.min.js';
-import './typeahead.bundle'
-import './jquery.easydropdown';
-import './easydrop';
-import './memenu';
-import './mem';
-import './responsiveslides.min';
-import './responsive';
-import './imagezoom';
-import './jquery.flexslider';
-import './flexs';
+
+
+// import './jqery-1.11.0.min.js'
+// import './bootstrap.min.js';
+// import './typeahead.bundle'
+// import './jquery.easydropdown';
+// import './easydrop';
+// import './memenu';
+// import './mem';
+// import './responsiveslides.min';
+// import './responsive';
+// import './imagezoom';
+// import './jquery.flexslider';
+// import './flexs';
--- watch_rails/config/webpack/environment.js
+++ watch_rails/config/webpack/environment.js
@@ -7,4 +7,19 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Popper: ['popper.js', 'default']
 }))
 
+environment.loaders.prepend('sass', {
+  test: /\.(css|scss|sass)$/,
+  use: [{
+      loader: 'style-loader'
+  }, {
+      loader: 'css-loader'
+  }, {
+      loader: 'sass-loader',
+      options: {
+          includePaths: ['node_modules'],
+      }
+  }]
+})
+
+
 module.exports = environment
