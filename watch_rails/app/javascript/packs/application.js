682683dea9335fc915860be1cdce0be7f479f566
SinkLineP <popovartem505@gmail.com>
2020-07-03 23:22:04 +0300

    fix styles

--- watch_rails/app/javascript/packs/application.js
+++ watch_rails/app/javascript/packs/application.js
@@ -13,7 +13,7 @@ import 'bootstrap'
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('./images', true)
+require.context('./images', true)
 // const imagePath = (name) => images(name, true)
 // import 'bootstrap/dist/js/bootstrap'
 // import 'bootstrap/dist/css/bootstrap'
