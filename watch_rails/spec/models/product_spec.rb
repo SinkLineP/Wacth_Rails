7b88d3ef3f643dc9dcc44e52166b235a2ea0386c
SinkLineP <popovartem505@gmail.com>
2020-08-11 15:06:18 +0300

    product

--- watch_rails/spec/models/product_spec.rb
+++ watch_rails/spec/models/product_spec.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
+
+  it { should have_many(:related_products) }
+  it { should have_many(:related) }
 end
