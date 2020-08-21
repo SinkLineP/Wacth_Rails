14118ae30f7d341dccee2ffa30ade287f6052eff
SinkLineP <popovartem505@gmail.com>
2020-08-21 09:44:37 +0300

    done rspec gallery

--- watch_rails/app/models/product.rb
+++ watch_rails/app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
-    validates :title, presence: true
-    validates :price, presence: true
+  validates :title, presence: true
+  validates :price, presence: true
+
   
-    has_many :related_products
-    has_many :related, through: :related_products
-  
-    enum hit: { hit: 1, not_hit: 0 }
-  end
+  has_many :galleries
+  has_many :related_products
+  has_many :related, through: :related_products
+
+  enum hit: { hit: 1, not_hit: 0 }
+end
