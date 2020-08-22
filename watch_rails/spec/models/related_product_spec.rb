4376a7aec15513295716a7e9c658d86eb77b05a0
SinkLineP <popovartem505@gmail.com>
2020-08-22 17:43:58 +0300

    15:52 rspec categories.rb

--- watch_rails/spec/models/related_product_spec.rb
+++ watch_rails/spec/models/related_product_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe RelatedProduct, type: :model do
   it { should belong_to(:product) }
-  it { should belong_to(:related) }
+  it { should belong_to(:related) } 
 end
