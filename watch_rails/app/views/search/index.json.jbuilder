2b1d8fb5c7e4ff75ad4f564de32ec5332b99c5ce
SinkLineP <popovartem505@gmail.com>
2020-09-09 18:46:38 +0300

    Lesson 8 Finished

--- watch_rails/app/views/search/index.json.jbuilder
+++ watch_rails/app/views/search/index.json.jbuilder
@@ -1,6 +1,7 @@
 json.products do
-    json.array!(@products) do |product|
-      json.title product.title
-      json.id    product.id
-    end
-  end    
+  json.array!(@products) do |product|
+    json.title product.title
+    #json.id    product.id
+    json.url   product_path(product)
+  end
+end    
--- watch_rails/spec/controllers/search_controller_spec.rb
+++ watch_rails/spec/controllers/search_controller_spec.rb
@@ -1,31 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
-	render_views
+  render_views
 
-	describe "GET #index" do
-		let!(:product) { create(:product, title: "Casio LED") }
-		let(:query) { "cas" }
-		subject!{ get :index, params: { query: query, format: :json } }
+  describe "GET #index" do
+    let!(:product) { create(:product, title: "polroy LED") }
+    let(:q) { "roy" }
+    subject!{ get :index, params: { q: q, format: :json } }
 
-		context "search product" do
-			it "find product in db" do
-				res = JSON.parse(response.body)
+    context "search product" do
+      it "find product in db" do
+        parse_body = JSON.parse(response.body)
+        expect(parse_body).to eq("products" => ["title" => product.title, "url" => "/product/#{product.id}"])
+      end
 
-				expect(res).to eq(product.title, product.id)
-			end
-		
+      it "response with 200 ok" do
+        expect(response.code).to eq('200')
+      end
 
-			it "response with 200 ok" do
-				expect(response.code).to eq('200')
-			end
-
-			context "search with wrong value" do
-				let(:query) { 'wrong' }
-				it "nothing found" do
-					expect response.body.empty?  
-				end 
-			end         
-		end    
-	end
+      context "search with wrong value" do
+        let(:query) { 'wrong' }
+        it "nothing found" do
+          expect response.body.empty?  
+        end          
+      end            
+    end
+  end  
 end
