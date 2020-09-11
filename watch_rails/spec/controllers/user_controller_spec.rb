75a1db78c377779cc0f900d3d5706333a6b8ed10
SinkLineP <popovartem505@gmail.com>
2020-09-11 13:01:21 +0300

    lesson 9 Finished

---
+++ watch_rails/spec/controllers/user_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+    render_views
+
+    let!(:user) { create :user  } 
+    let(:product) { create :product }
+    let(:cart) { create :cart, user: user }
+    let!(:item) { create :cart_item, cart: cart, product: product }   
+
+    describe "GET #show" do
+        subject { get :show } 
+
+        it "render show view" do
+          sign_in(user)
+          is_expected.to render_template :show
+          expect(response.body).to include(product.title)
+        end   
+    end
+end
